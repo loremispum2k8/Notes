162e14d20d58fe41eba0b6ded39513076f1486af
Loremispum <141552551+loremispum2k8@users.noreply.github.com>
2025-04-19 00:26:03 +0300

    Add the add popup

---
+++ script.js
@@ -0,0 +1,10 @@
+let card = document.querySelectorAll('.notes-card');
+let canvas = document.querySelector('.canvas');
+
+let addBtn = document.querySelector('.add');
+let deleteBtn = document.querySelector('.delete');
+let notesArray = [];
+addBtn.addEventListener('click',()=>{
+    
+})
+
